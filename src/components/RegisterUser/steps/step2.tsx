0c33bbd583023379ba838f839946f0fcc483073a
Anthony <anthonyfdez23@gmail.com>
2023-03-10 14:18:52 -0500

    Add

--- src/components/RegisterUser/steps/step2.tsx
+++ src/components/RegisterUser/steps/step2.tsx
@@ -8,73 +8,119 @@ import {
   Button,
   Flex,
   Divider,
+  Avatar,
 } from "@mantine/core";
-import { ArrowLeft, Flag, Image, Upload, X } from "react-feather";
+import { ArrowLeft, Flag, Image, Trash, Upload, X } from "react-feather";
 import { useForm } from "react-hook-form";
 import { IStepProps } from "../RegisterUser";
-
-interface IFormValues {
-  profileImage: File;
-}
+import { closeAllModals, openModal } from "@mantine/modals";
 
 const Step2 = ({ prevStep, nextStep, active }: IStepProps): JSX.Element => {
   const theme = useMantineTheme();
 
   const [profileImage, setProfileImage] = useState<File | null>(null);
 
+  const displayUploadedImage = (): JSX.Element => {
+    if (!profileImage) return <p>Unable to preview image.</p>;
+
+    const imagePreview = URL.createObjectURL(profileImage);
+
+    return (
+      <Flex
+        justify="center"
+        mt={20}
+      >
+        <Avatar
+          radius="50%"
+          size={250}
+          src={imagePreview}
+        />
+      </Flex>
+    );
+  };
+
   return (
     <div>
       <h1>Upload your profile image</h1>
-      <Dropzone
-        accept={IMAGE_MIME_TYPE}
-        maxSize={3 * 1024 ** 2}
-        multiple={false}
-        onDrop={(files) => console.log("accepted files", files)}
-        onReject={(files) => console.log("rejected files", files)}
-      >
-        <Group
-          position="center"
-          spacing="xl"
-          style={{ minHeight: rem(220), pointerEvents: "none" }}
+      {profileImage ? (
+        displayUploadedImage()
+      ) : (
+        <Dropzone
+          accept={IMAGE_MIME_TYPE}
+          maxSize={3 * 1024 ** 2}
+          multiple={false}
+          onDrop={(files): void => {
+            setProfileImage(files[0]);
+          }}
+          onReject={(): void => {
+            openModal({
+              title: "Unable to use that image",
+              children: (
+                <div>
+                  <p>Please be sure that the file you uploaded was an image</p>
+                  <Flex
+                    justify="flex-end"
+                    mt={20}
+                  >
+                    <Button
+                      onClick={(): void => {
+                        closeAllModals();
+                      }}
+                    >
+                      Close
+                    </Button>
+                  </Flex>
+                </div>
+              ),
+            });
+          }}
         >
-          <Dropzone.Accept>
-            <Upload
-              color={
-                theme.colors[theme.primaryColor][
-                  theme.colorScheme === "dark" ? 4 : 6
-                ]
-              }
-              size="3.2rem"
-            />
-          </Dropzone.Accept>
-          <Dropzone.Reject>
-            <X
-              color={theme.colors.red[theme.colorScheme === "dark" ? 4 : 6]}
-              size="3.2rem"
-            />
-          </Dropzone.Reject>
-          <Dropzone.Idle>
-            <Image size="3.2rem" />
-          </Dropzone.Idle>
+          <Group
+            position="center"
+            spacing="xl"
+            style={{ minHeight: rem(220), pointerEvents: "none" }}
+          >
+            <Dropzone.Accept>
+              <Upload
+                color={
+                  theme.colors[theme.primaryColor][
+                    theme.colorScheme === "dark" ? 4 : 6
+                  ]
+                }
+                size="3.2rem"
+              />
+            </Dropzone.Accept>
+            <Dropzone.Reject>
+              <X
+                color={theme.colors.red[theme.colorScheme === "dark" ? 4 : 6]}
+                size="3.2rem"
+              />
+            </Dropzone.Reject>
+            <Dropzone.Idle>
+              <Image size="3.2rem" />
+            </Dropzone.Idle>
 
-          <div>
-            <Text
-              inline
-              size="xl"
-            >
-              Drag images here or click to select files
-            </Text>
-            <Text
-              color="dimmed"
-              inline
-              mt={7}
-              size="sm"
-            >
-              Attach as many files as you like, each file should not exceed 5mb
-            </Text>
-          </div>
-        </Group>
-      </Dropzone>
+            <div>
+              <Text
+                inline
+                size="xl"
+              >
+                Drag images here or click to select files
+              </Text>
+              <Text
+                color="dimmed"
+                inline
+                mt={7}
+                size="sm"
+              >
+                Attach as many files as you like, each file should not exceed
+                5mb
+              </Text>
+            </div>
+          </Group>
+        </Dropzone>
+      )}
+
       <Divider
         mb={20}
         mt={20}
@@ -87,12 +133,28 @@ const Step2 = ({ prevStep, nextStep, active }: IStepProps): JSX.Element => {
         >
           Back
         </Button>
-        <Button
-          rightIcon={<Flag size={16} />}
-          type="submit"
-        >
-          Finish
-        </Button>
+        <Flex>
+          {profileImage && (
+            <Button
+              color="red"
+              leftIcon={<Trash size={16} />}
+              mr={10}
+              onClick={(): void => {
+                setProfileImage(null);
+              }}
+              variant="outline"
+            >
+              Remove Imge
+            </Button>
+          )}
+
+          <Button
+            rightIcon={<Flag size={16} />}
+            type="submit"
+          >
+            Finish
+          </Button>
+        </Flex>
       </Flex>
     </div>
   );
