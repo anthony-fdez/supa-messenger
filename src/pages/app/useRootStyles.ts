23a092ef857730b2aeb178a2aaf7095336a9b781
Anthony <anthonyfdez23@gmail.com>
2023-03-11 00:19:05 -0500

    Header styles

--- src/pages/app/useRootStyles.ts
+++ src/pages/app/useRootStyles.ts
@@ -23,6 +23,14 @@ const useRootStyles = createStyles((theme) => ({
     padding: 15,
     display: "flex",
     justifyContent: "space-between",
+    backgroundColor:
+      theme.colorScheme === "dark"
+        ? theme.colors.dark[9]
+        : theme.colors.gray[1],
+
+    borderBottom: `1px solid ${
+      theme.colorScheme === "dark" ? theme.colors.dark[5] : theme.colors.gray[3]
+    }`,
   },
 }));
 
