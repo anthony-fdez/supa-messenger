2d858b71355a0503a274bb7f4fcfc25786097665
Anthony Fernandez <anthonyfdez23@gmail.com>
2023-03-21 17:32:21 -0400

    Create room password ui

--- src/pages/app/Room/EnterRoomPassword/EnterRoomPassword.tsx
+++ src/pages/app/Room/EnterRoomPassword/EnterRoomPassword.tsx
@@ -1,9 +1,31 @@
+import { Button, Card, Flex, Text, TextInput } from "@mantine/core";
 import React from "react";
 
 const EnterRoomPassword = (): JSX.Element => {
   return (
     <div>
-      <h1>Enter Room password</h1>
+      <Card
+        sx={{ maxWidth: "500px", margin: "auto" }}
+        withBorder
+      >
+        <h1>Private Room</h1>
+        <Text c="dimmed">
+          This is a private room, enter the room password to join.
+        </Text>
+        <TextInput
+          label="Room Password"
+          mt={20}
+          placeholder="******"
+          type="password"
+          withAsterisk
+        />
+        <Flex
+          justify="flex-end"
+          mt={20}
+        >
+          <Button>Join Room</Button>
+        </Flex>
+      </Card>
     </div>
   );
 };
