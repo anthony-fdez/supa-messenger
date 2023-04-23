f69afeb811ce8968571793a98acc946a1125c876
Fidel <59423@bears.smcc.edu>
2023-04-23 02:31:34 -0400

    added: check if userid and message user id and also added hover card for icons

--- src/pages/app/Room/Messages/Message/Message.tsx
+++ src/pages/app/Room/Messages/Message/Message.tsx
@@ -1,12 +1,21 @@
 import React, { useState } from "react";
-import { ActionIcon, Flex, Loader, Text, Textarea } from "@mantine/core";
+import {
+  ActionIcon,
+  Flex,
+  HoverCard,
+  Loader,
+  Text,
+  Textarea,
+} from "@mantine/core";
 import moment from "moment";
 
 import { Send } from "react-feather";
 import { useSupabaseClient } from "@supabase/auth-helpers-react";
 import { showNotification } from "@mantine/notifications";
-import { useClickOutside } from "@mantine/hooks";
-import { IDatabaseMessages } from "../../../../../store/useGlobalStore";
+import { useClickOutside, useDisclosure } from "@mantine/hooks";
+import useGlobalStore, {
+  IDatabaseMessages,
+} from "../../../../../store/useGlobalStore";
 import useMessageStyles from "../useMessageStyles";
 import UserPopup from "../../../../../components/UserPopup/UserPopup";
 import UserAvatarWithIndicator from "../../../../../components/UserAvatarWithIndicator/UserAvatarWithIndicator";
@@ -24,6 +33,8 @@ const Message = ({ message }: Props): JSX.Element => {
   const [isEditingMessage, setIsEditingMessage] = useState(false);
   const [editMessage, setEditMessage] = useState("");
   const [isSendingMessage, setIsSendingMessage] = useState(false);
+  const [opened, { close, open }] = useDisclosure(false);
+  const { user } = useGlobalStore();
 
   const outsideClickRef = useClickOutside(() => setIsEditingMessage(false));
 
@@ -63,88 +74,167 @@ const Message = ({ message }: Props): JSX.Element => {
   };
 
   return (
-    <Flex
-      key={message.id}
-      mb={10}
-      className={classes.messageDiv}
-      justify="space-between"
-    >
-      <Flex>
-        <div className={classes.avatarDiv}>
-          <UserPopup
-            user={{
-              // @ts-ignore
-              email: message.userData.email || "",
-              // @ts-ignore
-              id: message.userData.id || "",
-              // @ts-ignore
-              imageUrl: message.userData.image_url || "",
-              // @ts-ignore
-              name: message.userData.name || "",
-            }}
-          >
-            <UserAvatarWithIndicator
-              // @ts-ignore
-              user_email={message.userData.email}
-              size={30}
-              // @ts-ignore
-              image={message.userData.image_url}
-            />
-          </UserPopup>
-        </div>
-        <div style={{ marginLeft: 10 }}>
-          <Text
-            c="dimmed"
-            size={14}
-          >
-            {/* @ts-ignore */}
-            {`${message.userData.name} - ${moment(
-              message.created_at,
-            ).fromNow()}`}
-          </Text>
-          {isEditingMessage ? (
-            <form
-              ref={outsideClickRef}
-              onSubmit={(e): void => {
-                e.preventDefault();
-                handleEdit(message);
-              }}
+    <>
+      {!isEditingMessage && user.uid === message.user_id ? (
+        <HoverCard shadow="md" position="top-end">
+          <HoverCard.Target>
+            <Flex
+              key={message.id}
+              mb={10}
+              className={classes.messageDiv}
+              justify="space-between"
             >
-              <Textarea
-                sx={{ maxWidth: "calc(100vw - 100px)", width: 600 }}
-                autosize
-                className={classes.edit_input}
-                onChange={
-                  (event): void =>
-                    // eslint-disable-next-line max-len
-                    // eslint-disable-next-line react/jsx-curly-newline, implicit-arrow-linebreak
-                    setEditMessage(event.target.value)
-                  // eslint-disable-next-line react/jsx-curly-newline
-                }
-                placeholder="Send message"
-                rightSection={sendButton()}
-                value={editMessage}
-                spellCheck="false"
-                autoComplete="off"
+              <Flex>
+                <div className={classes.avatarDiv}>
+                  <UserPopup
+                    user={{
+                      // @ts-ignore
+                      email: message.userData.email || "",
+                      // @ts-ignore
+                      id: message.userData.id || "",
+                      // @ts-ignore
+                      imageUrl: message.userData.image_url || "",
+                      // @ts-ignore
+                      name: message.userData.name || "",
+                    }}
+                  >
+                    <UserAvatarWithIndicator
+                      // @ts-ignore
+                      user_email={message.userData.email}
+                      size={30}
+                      // @ts-ignore
+                      image={message.userData.image_url}
+                    />
+                  </UserPopup>
+                </div>
+                <div style={{ marginLeft: 10 }}>
+                  <Text
+                    c="dimmed"
+                    size={14}
+                  >
+                    {/* @ts-ignore */}
+                    {`${message.userData.name} - ${moment(
+                      message.created_at,
+                    ).fromNow()}`}
+                  </Text>
+                  {isEditingMessage ? (
+                    <form
+                      ref={outsideClickRef}
+                      onSubmit={(e): void => {
+                        e.preventDefault();
+                        handleEdit(message);
+                      }}
+                    >
+                      <Textarea
+                        sx={{ maxWidth: "calc(100vw - 100px)", width: 600 }}
+                        autosize
+                        className={classes.edit_input}
+                        onChange={
+                          (event): void =>
+                            // eslint-disable-next-line max-len
+                            // eslint-disable-next-line react/jsx-curly-newline, implicit-arrow-linebreak
+                            setEditMessage(event.target.value)
+                          // eslint-disable-next-line react/jsx-curly-newline
+                        }
+                        placeholder="Send message"
+                        rightSection={sendButton()}
+                        value={editMessage}
+                        spellCheck="false"
+                        autoComplete="off"
+                      />
+                    </form>
+                  ) : (
+                    <Text>{message.message_body}</Text>
+                  )}
+                </div>
+              </Flex>
+            </Flex>
+          </HoverCard.Target>
+          <HoverCard.Dropdown>
+            <div className={classes.messageFunctionsDiv}>
+              <MessageFunctions
+                handleEdit={() => {
+                  setEditMessage(message.message_body);
+                  setIsEditingMessage(true);
+                }}
               />
-            </form>
-          ) : (
-            <Text>{message.message_body}</Text>
-          )}
-        </div>
-      </Flex>
-
-      {!isEditingMessage && (
-        <div className={classes.messageFunctionsDiv}>
-          <MessageFunctions
-            handleEdit={() => {
-              setEditMessage(message.message_body);
-              setIsEditingMessage(true);
-            }}
-          />
-        </div>
+            </div>
+          </HoverCard.Dropdown>
+        </HoverCard>
+      ) : (
+        <Flex
+          key={message.id}
+          mb={10}
+          className={classes.messageDiv}
+          justify="space-between"
+        >
+          <Flex>
+            <div className={classes.avatarDiv}>
+              <UserPopup
+                user={{
+                  // @ts-ignore
+                  email: message.userData.email || "",
+                  // @ts-ignore
+                  id: message.userData.id || "",
+                  // @ts-ignore
+                  imageUrl: message.userData.image_url || "",
+                  // @ts-ignore
+                  name: message.userData.name || "",
+                }}
+              >
+                <UserAvatarWithIndicator
+                  // @ts-ignore
+                  user_email={message.userData.email}
+                  size={30}
+                  // @ts-ignore
+                  image={message.userData.image_url}
+                />
+              </UserPopup>
+            </div>
+            <div style={{ marginLeft: 10 }}>
+              <Text
+                c="dimmed"
+                size={14}
+              >
+                {/* @ts-ignore */}
+                {`${message.userData.name} - ${moment(
+                  message.created_at,
+                ).fromNow()}`}
+              </Text>
+              {isEditingMessage ? (
+                <form
+                  ref={outsideClickRef}
+                  onSubmit={(e): void => {
+                    e.preventDefault();
+                    handleEdit(message);
+                  }}
+                >
+                  <Textarea
+                    sx={{ maxWidth: "calc(100vw - 100px)", width: 600 }}
+                    autosize
+                    className={classes.edit_input}
+                    onChange={
+                      (event): void =>
+                        // eslint-disable-next-line max-len
+                        // eslint-disable-next-line react/jsx-curly-newline, implicit-arrow-linebreak
+                        setEditMessage(event.target.value)
+                      // eslint-disable-next-line react/jsx-curly-newline
+                    }
+                    placeholder="Send message"
+                    rightSection={sendButton()}
+                    value={editMessage}
+                    spellCheck="false"
+                    autoComplete="off"
+                  />
+                </form>
+              ) : (
+                <Text>{message.message_body}</Text>
+              )}
+            </div>
+          </Flex>
+        </Flex>
       )}
-    </Flex>
+    </>
   );
 };
 
