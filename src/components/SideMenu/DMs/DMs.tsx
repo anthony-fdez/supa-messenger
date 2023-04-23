11a53fa5cfdbae3867cd86f568f96fd79db6d642
Anthony Fernandez <anthonyfdez23@gmail.com>
2023-04-22 20:17:55 -0400

    Add dms

--- src/components/SideMenu/DMs/DMs.tsx
+++ src/components/SideMenu/DMs/DMs.tsx
@@ -2,45 +2,72 @@ import React from "react";
 import { Text } from "@mantine/core";
 import { useNavigate } from "react-router";
 import useGlobalStore, {
-  IDatabaseParticipants,
+  IDatabaseUser,
+  IFriend,
 } from "../../../store/useGlobalStore";
 import useSideMenuStyles from "../SideMenu.styles";
+import getFriend from "../../../utils/friendships/getFriend";
+import UserAvatarWithIndicator from "../../UserAvatarWithIndicator/UserAvatarWithIndicator";
 
 const DMs = (): JSX.Element => {
   const { classes, cx } = useSideMenuStyles();
-  const { app, setApp, dms } = useGlobalStore();
+  const {
+    app,
+    setApp,
+    dms,
+    user: { uid },
+  } = useGlobalStore();
   const navigate = useNavigate();
 
-  const getFriendName = (participants: IDatabaseParticipants[]) => {
-    console.log(participants);
+  const getFriendData = (friendship: IFriend[]): IDatabaseUser | null => {
+    if (friendship.length === 0) return null;
 
-    if (participants.length !== 2) return "Error";
+    const friend = getFriend({ friendship: friendship[0], userId: uid || "" });
 
-    return "";
+    if (!friend || !friend.friendData) return null;
+
+    return friend.friendData;
   };
 
   return (
     <div>
-      {dms.map((room) => (
-        <a
-          key={room.id}
-          className={cx(classes.link, {
-            [classes.linkActive]:
-              app.secondaryActiveSideMenu === room.id.toString(),
-          })}
-          href="/"
-          onClick={(event): void => {
-            event.preventDefault();
-            setApp({
-              secondaryActiveSideMenu: room.id.toString(),
-              isMobileMenuOpen: false,
-            });
-            navigate(`/chat/${room.id}`);
-          }}
-        >
-          <Text lineClamp={1}>{getFriendName(room.participants)}</Text>
-        </a>
-      ))}
+      {dms.map((room) => {
+        const friendData = getFriendData(room.friendships);
+
+        if (!friendData) return null;
+
+        return (
+          <a
+            key={room.id}
+            className={cx(classes.link, {
+              [classes.linkActive]:
+                app.secondaryActiveSideMenu === room.id.toString(),
+            })}
+            href="/"
+            onClick={(event): void => {
+              event.preventDefault();
+              setApp({
+                secondaryActiveSideMenu: room.id.toString(),
+                isMobileMenuOpen: false,
+              });
+              navigate(`/chat/${room.id}`);
+            }}
+          >
+            <UserAvatarWithIndicator
+              checkOnline
+              size={35}
+              user_email={friendData?.email || "Error"}
+              image={friendData?.image_url || ""}
+            />
+            <Text
+              ml={10}
+              lineClamp={1}
+            >
+              {friendData?.name || "Error"}
+            </Text>
+          </a>
+        );
+      })}
     </div>
   );
 };
