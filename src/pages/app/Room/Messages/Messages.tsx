7c3d32d5e18576a8f7f215dcb81927c7cc1b68fd
Anthony Fernandez <anthonyfdez23@gmail.com>
2023-04-25 10:43:12 -0400

    Animated!

--- src/pages/app/Room/Messages/Messages.tsx
+++ src/pages/app/Room/Messages/Messages.tsx
@@ -1,5 +1,5 @@
-import { Box, ScrollArea } from "@mantine/core";
-import React, { useEffect, useRef } from "react";
+import { Box, Collapse, Divider, ScrollArea } from "@mantine/core";
+import React, { useEffect, useRef, useState } from "react";
 import useGlobalStore from "../../../../store/useGlobalStore";
 
 import EmptyRoom from "../../../../components/InfoScreens/EmptyRoom";
@@ -13,12 +13,44 @@ const Messages = (): JSX.Element => {
   };
 
   const {
-    currentRoom: { messages },
+    currentRoom: { messages, roomParticipants },
+    user: { uid },
   } = useGlobalStore();
+
+  const [lastMessageRead, setLastMessageRead] = useState<number | null>(null);
+  const [showLastReadLabel, setShowLastReadLabel] = useState(true);
 
   useEffect(() => {
     scrollToBottom();
   }, [messages?.length]);
+
+  useEffect(() => {
+    if (!roomParticipants) return;
+    if (roomParticipants.length === 0) return;
+
+    const me = roomParticipants.find(
+      (participant) => participant.user_id === uid,
+    );
+
+    if (me) {
+      setLastMessageRead(me.last_message_read);
+    }
+  }, [roomParticipants, uid, messages]);
+
+  useEffect(() => {
+    if (!lastMessageRead) {
+      return;
+    }
+
+    const timeout = setTimeout(() => {
+      setShowLastReadLabel(false);
+    }, 5000);
+
+    // eslint-disable-next-line consistent-return
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [lastMessageRead]);
 
   if (!messages) return <p>Error loading messages</p>;
   if (messages.length === 0) return <EmptyRoom />;
@@ -31,10 +63,27 @@ const Messages = (): JSX.Element => {
       <Box>
         {messages.map((message) => {
           return (
-            <Message
-              key={message.id}
-              message={message}
-            />
+            <>
+              <Message
+                key={message.id}
+                message={message}
+              />
+              <Collapse
+                in={
+                  lastMessageRead === message.id &&
+                  lastMessageRead !== messages[messages.length - 1].id &&
+                  showLastReadLabel
+                }
+              >
+                <Divider
+                  mt={10}
+                  mb={20}
+                  labelPosition="left"
+                  label="NEW"
+                  color="red"
+                />
+              </Collapse>
+            </>
           );
         })}
         <div ref={messagesEndRef} />
