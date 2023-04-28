3abbbc46cc7fb55c31bf923eb742d0d5ccc6c403
Anthony Fernandez <anthonyfdez23@gmail.com>
2023-04-28 14:40:59 -0400

    Remove base

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/supabase-chat/",
+  base: "/",
   server: {
     port: 3000,
   },
